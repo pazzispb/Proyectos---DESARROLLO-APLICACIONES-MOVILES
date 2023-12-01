39dd452c27a30bcd9e0aaabd88ad568f963a50ef
pazzispb20 <pazzispb20@gmail.com>
2023-12-01 15:20:49 -0400

    Primer commit del proyecto

---
+++ agenda/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:agenda/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(Agenda());
+
+class Agenda extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      home: HomeScreen(),
+    );
+  }
+}
