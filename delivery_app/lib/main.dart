15a7a1e149cc7fad186a391b992787faa4885850
pazzispb20 <pazzispb20@gmail.com>
2023-12-13 08:39:25 -0400

    Archivos agregados

---
+++ delivery_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:delivery_app/pages/login/login_page.dart';
+import 'package:delivery_app/pages/register/register_page.dart';
+import 'package:delivery_app/utils/my_colors.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      theme: ThemeData(
+        useMaterial3: false,
+        colorScheme: ColorScheme.fromSeed(seedColor: MyColors.primaryColor, brightness: Brightness.light)
+      ),
+      initialRoute: 'login',
+      routes: {
+        'login': (context) => const LoginPage(),
+        'register': (context) => const RegisterPage()
+      },
+    );
+  }
+}
